3cd232ee750418b83d20a65bf5f0845406a1f8c2
YNNJN <60057425+YNNJN@users.noreply.github.com>
2020-04-03 15:48:32 +0900

    D3

--- 5105_미로의거리.py
+++ 5105_미로의거리.py
@@ -1,40 +1,39 @@
-#최소 몇 개의 칸을 지나야 출발지에서 도착지에 다다를 수 있는지
-def find(i, j):
+def bfs(si,sj):
     global cnt
-    q = 0
     dr = [-1, 0, 1, 0]
     dc = [0, 1, 0, -1]
-    s = [[i, j, q]]
-    while s:
-        [pi, pj, q] = s.pop()
+    q.append((si, sj))
+    visited.append((si, sj))
+
+    while q:
+        si, sj = q.pop(0)
         for k in range(4):
-            nr = pi + dr[k]
-            nc = pj + dc[k]
+            nr = si + dr[k]
+            nc = sj + dc[k]
             if nr < 0 or nr >= n or nc < 0 or nc >= n: continue
             if maze[nr][nc] == 1: continue
-            if maze[nr][nc] == 3:
-                cnt.append(q)
-                break
-            if maze[nr][nc] == 5: continue
-            maze[pi][pj] = 5
-            q += 1
-            s.append([nr, nc, q])
-            q -= 1
-    return
+            if (nr,nc) not in visited:
+                q.append((nr, nc))
+                visited.append((nr, nc))
+                cnt_list[nr][nc] = cnt_list[si][sj] + 1
+                if maze[nr][nc] == 3:
+                    cnt = cnt_list[nr][nc] - 1
+                    return
 
 
 for tc in range(int(input())):
     n = int(input())
     maze = [list(map(int, input())) for _ in range(n)]
-    cnt = []
+    visited = []
 
     for i in range(n):
         for j in range(n):
             if maze[i][j] == 2:
-                si, sj = i, j
+                si, sj = i,j
                 break
-    find(si, sj)
-    if len(cnt) != 0:
-        print('#{} {}'.format(tc+1, min(cnt)))
-    else:
-        print('#{} {}'.format(tc+1, 0))
+
+    cnt = 0
+    cnt_list = [[0] * n for _ in range(n)]
+    q = []
+    bfs(si,sj)
+    print('#{} {}'.format(tc+1, cnt))
