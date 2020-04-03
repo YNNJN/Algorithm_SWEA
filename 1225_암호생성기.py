e58217ef72f72d2f1b0609592a24edef62b6e2e2
YNNJN <60057425+YNNJN@users.noreply.github.com>
2020-04-03 16:37:35 +0900

    D3

--- 1225_암호생성기.py
+++ 1225_암호생성기.py
@@ -17,25 +17,42 @@
 #     print('#{} {}'.format(tc, ' '.join(map(str, data))))
 
 
-n = 8
-for t in range(1, 11):
-    tc = int(input())
-    nums = list(map(int, input().split()))
-    flag = 1
-    while flag:
-        for i in range(1, 6):
-            tmp = nums[0]
-            for j in range(1, n):
-                nums[j-1] = nums[j]
-            nums[-1] = tmp - i
-            if nums[-1] < 1:
-                nums[-1] = 0
-                flag = 0
-                break
-    print('#{0}'.format(t), end=' ')
-    for num in nums:
-        print(num, end=' ')
+# n = 8
+# for t in range(1, 11):
+#     tc = int(input())
+#     nums = list(map(int, input().split()))
+#     flag = 1
+#     while flag:
+#         for i in range(1, 6):
+#             tmp = nums[0]
+#             for j in range(1, n):
+#                 nums[j-1] = nums[j]
+#             nums[-1] = tmp - i
+#             if nums[-1] < 1:
+#                 nums[-1] = 0
+#                 flag = 0
+#                 break
+#     print('#{0}'.format(t), end=' ')
+#     for num in nums:
+#         print(num, end=' ')
 
+
+# #queue 이용
+# for _ in range(1, 11):
+#     tc = int(input())
+#     que = list(map(int, input().split()))
+#     dist = 0
+#     while True:
+#         num = que.pop(0)
+#         dec = dist % 5 + 1 #감소하는 숫자가 1, 2, 3, 4, 5, 다시 1로 변함
+#         num -= dec
+#         if num <= 0:
+#             que.append(0)
+#             break
+#         que.append(num)
+#         dist += 1
+#     print('#{}'.format(tc), end =' ')
+#     print(' '.join(map(str, que)))
 
 
 #젤 간단, 0의 개수 세서 풀기
--- 5099_피자굽기.py
+++ 5099_피자굽기.py
@@ -1,18 +1,42 @@
+# for tc in range(int(input())):
+#     n,m = map(int, input().split())
+#     c = list(map(int, input().split()))
+#     arr = [[c[i], i+1] for i in range(n)]
+#     i = n
+#     while len(arr) > 1:
+#         if arr[0][0] != 0:
+#             arr[0][0] //= 2
+#             if arr[0][0] == 0:
+#                 arr.pop(0)
+#                 if i < m:
+#                     arr.append([c[i], i+1])
+#                     i += 1
+#             else:
+#                 x = arr.pop(0)
+#                 arr.append(x)
+#
+#     print('#{} {}'.format(tc+1, arr[0][1]))
+
+
+#큐 이용
+import queue
+def find():
+    pot = queue.Queue()
+    for i in range(1, n+1):
+        pot.put(i)
+    idx = n + 1 #아직 화덕에 넣지 않은 피자 번호
+    t = 0 #피자 인덱스
+    while pot.empty() == False:
+        t = pot.get()
+        if arr[t]//2 != 0:
+            arr[t] //= 2
+            pot.put(t)
+        elif idx <= m:
+            pot.put(idx)
+            idx += 1
+    return t #마지막으로 남은 피자 번호
+
 for tc in range(int(input())):
     n,m = map(int, input().split())
-    c = list(map(int, input().split()))
-    arr = [[c[i], i+1] for i in range(n)]
-    i = n
-    while len(arr) > 1:
-        if arr[0][0] != 0:
-            arr[0][0] //= 2
-            if arr[0][0] == 0:
-                arr.pop(0)
-                if i < m:
-                    arr.append([c[i], i+1])
-                    i += 1
-            else:
-                x = arr.pop(0)
-                arr.append(x)
-
-    print('#{} {}'.format(tc+1, arr[0][1]))
+    arr = [0] + list(map(int, input().split()))
+    print('#{} {}'.format(tc+1, find()))
